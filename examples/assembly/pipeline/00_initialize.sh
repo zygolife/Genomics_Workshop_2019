6aad18d3bdeb9a5135e62af324fcd5db7b2edefb
Jason Stajich <jasonstajich.phd@gmail.com>
2019-09-10 12:06:14 -0700

    add logs to generate

--- examples/assembly/pipeline/00_initialize.sh
+++ examples/assembly/pipeline/00_initialize.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 #SBATCH -N 1 -n 1 -p short --mem 1gb  --out logs/init.log
 
-mkdir -p input
+mkdir -p input logs
 pushd input
 ln -s /bigdata/stajichlab/shared/tmp/Afum/DRR022920/* .
 ln -s /bigdata/stajichlab/shared/tmp/Afum/DRR022919/* .
