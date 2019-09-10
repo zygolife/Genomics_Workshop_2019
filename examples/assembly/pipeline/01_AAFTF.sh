37beb819909f1ea43782e99b95ffc417ae80f6e1
Jason Stajich <jason.stajich@gmail.com>
2019-09-09 21:26:04 -0700

    format

--- examples/assembly/pipeline/01_AAFTF.sh
+++ examples/assembly/pipeline/01_AAFTF.sh
@@ -51,43 +51,43 @@ do
 	    fi
 	fi
 	AAFTF assemble -c $CPU --left $LEFT --right $RIGHT  --memory $MEM \
-	    -o $ASMFILE -w $WORKDIR/spades_${STRAIN}
-
+	      -o $ASMFILE -w $WORKDIR/spades_${STRAIN}
+	
 	if [ -s $ASMFILE ]; then
 	    rm -rf $WORKDIR/spades_${STRAIN}/K?? $WORKDIR/spades_${STRAIN}/tmp
 	fi
-
+	
 	if [ ! -f $ASMFILE ]; then
 	    echo "SPADES must have failed, exiting"
 	    exit
 	fi
     fi
-
+    
     if [ ! -f $VECCLEAN ]; then
 	AAFTF vecscreen -i $ASMFILE -c $CPU -o $VECCLEAN
     fi
-        if [ ! -f $PURGE ]; then
-	AAFTF sourpurge -i $VECCLEAN -o $PURGE -c $CPU --phylum $PHYLUM --left $LEFT  --right $RIGHT
+    if [ ! -f $PURGE ]; then
+	AAFTF sourpurge -i $VECCLEAN -o $PURGE -c $CPU --phylum $PHYLUM --left $LEFT --right $RIGHT
     fi
-
-#    if [ ! -f $CLEANDUP ]; then
-#	AAFTF rmdup -i $PURGE -o $CLEANDUP -c $CPU -m 500
-#    fi
-
-#    if [ ! -f $PILON ]; then
-#	AAFTF pilon -i $CLEANDUP -o $PILON -c $CPU --left $LEFT  --right $RIGHT
-#    fi
-
-#    if [ ! -f $PILON ]; then
-#	echo "Error running Pilon, did not create file. Exiting"
-#	exit
-#    fi
-
+    
+    #    if [ ! -f $CLEANDUP ]; then
+    #	AAFTF rmdup -i $PURGE -o $CLEANDUP -c $CPU -m 500
+    #    fi
+    
+    #    if [ ! -f $PILON ]; then
+    #	AAFTF pilon -i $CLEANDUP -o $PILON -c $CPU --left $LEFT  --right $RIGHT
+    #    fi
+    
+    #    if [ ! -f $PILON ]; then
+    #	echo "Error running Pilon, did not create file. Exiting"
+    #	exit
+    #    fi
+    
     if [ ! -f $SORTED ]; then
 	# AAFTF sort -i $PILON -o $SORTED
 	AAFTF sort -i $VECCLEAN -o $SORTED
     fi
-
+    
     if [ ! -f $STATS ]; then
 	AAFTF assess -i $SORTED -r $STATS
     fi
