ca42d59721e085a0aa4fe7fde07469d704d65adf
Jason Stajich <jasonstajich.phd@gmail.com>
2019-09-09 21:20:22 -0700

    add pipeline example

---
+++ examples/assembly/pipeline/00_initialize.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/bash
+#SBATCH -N 1 -n 1 -p short --mem 1gb  --out logs/init.log
+
+mkdir -p input
+pushd input
+ln -s /bigdata/stajichlab/shared/tmp/Afum/DRR022920/* .
+ln -s /bigdata/stajichlab/shared/tmp/Afum/DRR022919/* .
+popd
