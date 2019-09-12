e2fce5ddcea53db4821788b16edd6bdb236a5a2a
Jason Stajich <jasonstajich.phd@gmail.com>
2019-09-12 00:45:13 -0700

    dynamic determine threads

--- examples/variant/pipeline/01_bwa.sh
+++ examples/variant/pipeline/01_bwa.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/bash
-#SBATCH -p short -N 1 -n 8 --mem 8gb --out logs/bwa.%a.log
+#SBATCH -p short -N 1 -n 8 --mem 8gb --out logs/bwa.%a.log --time 2:00:00
 
-CPU=8
+CPU=1
+if [ $SLURM_CPUS_ON_NODE ]; then
+ CPU=$SLURM_CPUS_ON_NODE
+fi
 
 module load bwa
 module load samtools
@@ -15,14 +18,17 @@ mkdir -p $OUTPUT
 IFS=,
 sed -n ${N}p $SAMPLEFILE | while read BASE STRAIN
 do
+    if [ ! -f $OUTPUT/$STRAIN.bam ]; then
+	
 	if [ ! -f $OUTPUT/$STRAIN.sam ]; then
-		bwa mem -t $CPU genome/Scerevisiae.fasta \
+	    bwa mem -t $CPU genome/Scerevisiae.fasta \
 		$INPUT/${BASE}_1.fastq.gz $INPUT/${BASE}_2.fastq.gz > $OUTPUT/$STRAIN.sam 
 	fi
-	if [ ! -f $OUTPUT/$STRAIN.bam ]; then
-		samtools sort -O bam -o $OUTPUT/$STRAIN.bam --threads $CPU -T /scratch $OUTPUT/$STRAIN.sam
-	fi
+	samtools sort -O bam -o $OUTPUT/$STRAIN.bam --threads $CPU -T /scratch $OUTPUT/$STRAIN.sam
+	
 	if [ ! -f $OUTPUT/$STRAIN.bam.bai ]; then
-		samtools index $OUTPUT/$STRAIN.bam
+	    samtools index $OUTPUT/$STRAIN.bam
 	fi
+	rm $OUTPUT/$STRAIN.sam
+    fi
 done
