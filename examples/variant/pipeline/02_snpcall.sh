32d36876c2248c7f6a0d447955d63bf339479fd0
Jason Stajich <jasonstajich.phd@gmail.com>
2019-09-12 00:45:26 -0700

    bayesian snp calling with bcftools

--- examples/variant/pipeline/02_snpcall.sh
+++ examples/variant/pipeline/02_snpcall.sh
@@ -3,8 +3,12 @@
 
 module load bcftools
 module load samtools
+CPU=1
+if [ $SLURM_CPUS_ON_NODE ]; then
+ CPU=$SLURM_CPUS_ON_NODE
+fi
 
 GENOME=genome/Scerevisiae.fasta
 ALN=aln
-bcftools mpileup -Ou -f $GENOME $ALN/*.bam  | bcftools call -vmO z -o called_variants.vcf.gz
+bcftools mpileup --threads $CPU -Ou -f $GENOME $ALN/*.bam  | bcftools call -vmO z -o called_variants.vcf.gz
 
