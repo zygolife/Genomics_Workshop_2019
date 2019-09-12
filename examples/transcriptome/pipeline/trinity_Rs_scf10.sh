c9760615c9818f5725f8f03645968d71c5bd8adc
Jason Stajich <jasonstajich.phd@gmail.com>
2019-09-11 22:57:54 -0700

    switch to batch queue by default

--- examples/transcriptome/pipeline/trinity_Rs_scf10.sh
+++ examples/transcriptome/pipeline/trinity_Rs_scf10.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/bash
-#SBATCH -p intel --mem 24gb -N 1 -n 16 --out trinity_scf10.log
+#SBATCH -p batch --mem 24gb -N 1 -n 16 --out trinity_scf10.log
 module load trinity-rnaseq
-Trinity --left input/Rs_scf10_1.fq.gz --right input/Rs_scf10_2.fq.gz --CPU 16 --max_memory 24G --seqType fq --output trinity_out_scf10
+Trinity --left input/Rs_scf10_1.fq.gz  --right input/Rs_scf10_2.fq.gz --CPU 16 --max_memory 24G --seqType fq --output trinity_out_scf10
