5f6481dd31b73fe1b22f94083fedfc4a83b9c706
Jason Stajich <jasonstajich.phd@gmail.com>
2019-09-13 15:19:53 -0700

    update

--- examples/bedtools/run_bedtools.sh
+++ examples/bedtools/run_bedtools.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/bash
 module load bedtools
 # This script will calculate the SNPs per gene by looking at intersection of the two files
-ln -s ../../data/genomic/C_lusitaniae.genotypes_all.selected.SNPONLY.lungonly.vcf.gz
+ln -s ../../data/genomic/C_lusitaniae.SampleA.vcf.gz
 ln -s ../../data/genomic/candida_lusitaniae_1.sorted.gff3
 # make a gene - only feature file
 grep -P "\tgene\t" candida_lusitaniae_1.sorted.gff3 > candida_lusitaniae_1.genes.gff3
 
-bedtools intersect -a candida_lusitaniae_1.genes.gff3 -b C_lusitaniae.genotypes_all.selected.SNPONLY.lungonly.vcf.gz -wb | cut -f9 | perl -p -e 's/ID.+Name=//' | sort | uniq -c | sort -rn > genes_by_snpcount.txt
+bedtools intersect -a candida_lusitaniae_1.genes.gff3 -b C_lusitaniae.SampleA.vcf.gz -wb | cut -f9 | perl -p -e 's/ID.+Name=//' | sort | uniq -c | sort -rn > genes_by_snpcount.txt
 
